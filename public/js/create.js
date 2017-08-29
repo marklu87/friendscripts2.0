cdb9470aa2293cfeb193f323d2600298952bc0b4
ccruml20 <ccrumley1@gmail.com>
2017-08-28 20:47:22 -0600

    some styling stuff

--- public/js/create.js
+++ public/js/create.js
@@ -1,6 +1,8 @@
 
  $(document).ready(function() {
 
+   alert("working");
+
   // Initial Values
    var name = "";
    var email = "";
@@ -88,3 +90,25 @@ $("#newStoryText").keyup(function() {
  })
 
 });
+
+
+
+//Join Story Div
+
+$.get("/api/stories", function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var storyDiv = $("<div>");
+      storyDiv.addClass("newStoryDiv");
+      storyDiv.attr("id", "storyID-" + data[i].id)
+      $(".title").append(storyDiv);
+
+      $("#storyID-" + data[i].id).append("<strong>Title:</strong> " + data[i].storyTitle + "<br>");
+      $("#storyID-" + data[i].id).append("<strong>Author:</strong> " + data[i].authorID + "<br>");
+      $("#storyID-" + data[i].id).append("<strong>Story Preview:</strong> "  + data[i].sentence);
+
+      $("#storyID-" + data[i].id).bind('click', {id: data[i].id}, (function(event) {
+        window.location.href = "http://localhost:8080/edit/" + event.target.id;
+      }));
+    }
+});
+
