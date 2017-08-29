78bc7324a9e7e50387a5080142be114b85ddbfc5
Jordan Reynolds <jordanreynolds34@gmail.com>
2017-08-28 23:32:58 -0600

    Still having issues with post to sentences with author id

--- models/newstory.js
+++ models/newstory.js
@@ -1,29 +1,26 @@
 module.exports = function(sequelize, DataTypes) {
-  var newStory = sequelize.define("newStory", {
+  var newStory = sequelize.define("stories", {
     storyTitle: {
       type: DataTypes.STRING,
-      allowNull: true,
-      // validate: {
-      //   len: [1]
-      // }
-    },
-    authorID: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      // len: [1]
+      allowNull: true
     },
     sentence: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      // len: [1]
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    authorID: {
+      type: DataTypes.STRING
     }
-  });
+    });
+  
 
   newStory.associate = function(models) {
-  	newStory.hasMany(models.newSentence, {
-  		// onDelete: "cascade"
-  	});
-  };
+  	newStory.belongsTo(models.authors, {
+      foreignKey: {
+        allowNull: false
+  	}
+  });
 
   return newStory;
+};
 };
