87ea29cbd2033cf12fa390f57ac44991dd620fec
Mark Marklu Minhthy <marklu87@gmail.com>
2017-08-29 19:09:07 -0600

    merged with master, fixed newstoryjs

--- models/newstory.js
+++ models/newstory.js
@@ -12,22 +12,14 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING
     }
     });
-  
+
 
   newStory.associate = function(models) {
-<<<<<<< HEAD
-  	newStory.hasMany(models.newSentence, {
-  		// onDelete: "cascade"
-      foreignKey: {allowNull: false}
-  	});
-  };
-=======
   	newStory.belongsTo(models.authors, {
       foreignKey: {
         allowNull: false
-  	}
+    	}
   });
->>>>>>> babd72062bf582264fa2cede0a768011e9af3278
 
   return newStory;
 };
