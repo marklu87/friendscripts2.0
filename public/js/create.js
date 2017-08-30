feee832c3fcd59e07cf38a93bbd31e624eca9f7e
ccruml20 <ccrumley1@gmail.com>
2017-08-30 12:08:40 -0600

    added edit styling and layout

--- public/js/create.js
+++ public/js/create.js
@@ -120,14 +120,14 @@ $.get("/api/stories", function(data) {
           $(".title").append(storyDiv);
 
           $("#storyID-" + data[i].id).append(`<h4 class="col-md-12">${data[i].authorID}</h4>`);
-    $("#storyID-" + data[i].id).append(`<h3 class="col-md-12">Story ID: ${data[i].id}</h3>`);
-    $("#storyID-" + data[i].id).append(`<p col-md-8 col-md-offset-2>${data[i].sentence}</p> <br>`);
-    $("#storyID-" + data[i].id).append(`<button id="joinScript" type="button" class="btn btn-primary btn-sm btn3d col-md-4" value="join">Join</button>`);
-    $("#storyID-" + data[i].id).append(`<button id="readScript" type="button" class="btn btn-info btn-sm btn3d col-md-4" value="read">Read</button>`);
-    // $("#storyID-" + data[i].id).append(`<div class="row"></div>`);
-    $("#storyID-" + data[i].id).bind('click', {id: data[i].id}, (function(event) {
-      window.location.href = "http://localhost:8080/edit/" + event.target.id;
-    }));
+          $("#storyID-" + data[i].id).append(`<h3 class="col-md-12">Story Title: ${data[i].storyTitle}</h3>`);
+          $("#storyID-" + data[i].id).append(`<p col-md-8 col-md-offset-2>${data[i].sentence}</p> <br>`);
+          $("#storyID-" + data[i].id).append(`<button id="joinScript-${data[i].id}" type="button" class="btn btn-primary btn-sm btn3d col-md-4" value="join">Join</button>`);
+          $("#storyID-" + data[i].id).append(`<button id="readScript-${data[i].id}" type="button" class="btn btn-info btn-sm btn3d col-md-4" value="read">Read</button>`);
+          // $("#storyID-" + data[i].id).append(`<div class="row"></div>`);
+          $("#storyID-" + data[i].id).bind('click', {id: data[i].id}, (function(event) {
+            window.location.href = "http://localhost:8080/edit/" + event.target.id;
+          }));
 
           // $("#storyID-" + data[i].id).append(`<h4 class="col-md-12">${data[i].storyTitle}</h4>`);
           // $("#storyID-" + data[i].id).append(`<h3 class="col-md-12">by: ${data[i].authorID}</h3>`);
