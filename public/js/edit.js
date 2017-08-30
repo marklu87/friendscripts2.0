0a8f829094cefd9295ddf72db53ce894e069a8ce
ccruml20 <ccrumley1@gmail.com>
2017-08-30 15:38:17 -0600

    commited marks changes to my local branch again

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,21 +1,48 @@
-alert("i'm working");
-
 $(document).ready(function() {
 
   var trimStoryID = window.location.pathname.toString();
-    trimStoryID = trimStoryID.substr(trimStoryID.indexOf("-") + 1)
-    console.log(trimStoryID);
+  trimStoryID = trimStoryID.substr(trimStoryID.indexOf("-") + 1)
+  console.log(trimStoryID);
   //Click Function to Post new sentence
   $(".commitNewSentence").click(function(){
     var newPost = {
-    sentence: $("#newStoryText").val().trim(),
-    storyId: trimStoryID
+      sentence: $("#newStoryText").val().trim(),
+      storyId: trimStoryID
     };
     $.post("/api/sentences", newPost, function() {
       // window.location.href = "/read";
-      }).done(function(data){
-          console.log(data);
-        });
+    }).done(function(data){
+      console.log(data);
+    });
   });
 
+
+
+  $.get("/api/stories", function(data) {
+    // console.log("stories data: " + data[1].id);
+    for (var i = 0; i < data.length; i++) {
+      var storyDivTwo = $(`<div class="col-md-12">`);
+      storyDivTwo.addClass("newStoryDivTwo");
+      storyDivTwo.attr("id", "storyID-" + data[i].id)
+      $(".sideTitle").append(storyDivTwo);
+    }
+  });
+
+  $.get("/api/stories", function(data) {
+    for (var i = 0; i < data.length; i++) {
+      var storyDiv = $(`<div class="col-md-3 storyStyle" >`);
+      storyDiv.addClass("newStoryDiv");
+      storyDiv.attr("id", "storyID-" + data[i].id)
+      $(".title").append(storyDiv);
+      $("#storyID-" + data[i].id).append(`<h4 class="col-md-12">${data[i].authorID}</h4>`);
+      $("#storyID-" + data[i].id).append(`<h3 class="col-md-12">Story Title: ${data[i].storyTitle}</h3>`);
+      $("#storyID-" + data[i].id).append(`<p col-md-8 col-md-offset-2>${data[i].sentence}</p> <br>`);
+      $("#storyID-" + data[i].id).append(`<button id="joinScript-${data[i].id}" type="button" class="btn btn-primary btn-sm btn3d col-md-4" value="join">Join</button>`);
+      $("#storyID-" + data[i].id).append(`<button id="readScript-${data[i].id}" type="button" class="btn btn-info btn-sm btn3d col-md-4" value="read">Read</button>`);
+      // $("#storyID-" + data[i].id).append(`<div class="row"></div>`);
+      $("#storyID-" + data[i].id).bind('click', {id: data[i].id}, (function(event) {
+        window.location.href = "http://localhost:8080/edit/" + event.target.id;
+      }));
+    }
+  });
 });
