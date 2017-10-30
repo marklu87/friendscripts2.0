910ae6cf36bbf2577079705ce3bef64c7b320b36
Mark Marklu Minhthy <marklu87@gmail.com>
2017-10-29 21:06:35 -0600

    changed signup to href. fixed overlap on new story page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,38 +1,30 @@
 var db = require("../models");
 var path = require("path");
-var passport = require('passport');
- module.exports = function(app) {
+var passport = require("passport");
+module.exports = function(app) {
+	//sign up get route
+	app.get("/auth/signup", function(req, res, next) {
+		res.sendFile(path.join(__dirname, "../public/signup.html"));
+	});
+	app.get("/auth/profile", function(req, res, next) {
+		console.log("here");
+		res.json(req.user);
+	});
 
+	//sign up post route
+	app.post(
+		"/auth/signup",
+		passport.authenticate("local-signup", {
+			successRedirect: "/newStory",
+			failureRedirect: "/auth/signup"
+		})
+	);
 
-
-//sign up get route
-	app.get('/auth/signup', function(req, res, next) {
- 		res.sendFile(path.join(__dirname, "../public/signUp.html"));
-
- 	});
-	app.get('/auth/profile', function(req, res, next) {
-    console.log("here");
- 		res.json(req.user);
-
-
-
- 	});
-
-//sign up post route
- 	app.post("/auth/signup", passport.authenticate('local-signup', {
-        successRedirect: '/newStory'
-        ,
-        failureRedirect: '/auth/signup'
-    }
-    ));
-
-
-  app.post("/auth/signin", passport.authenticate('local-signin', {
-        successRedirect: '/newStory',
-        failureRedirect: '/auth/signup'
-    })
-
-      );
-
-
- 	};
+	app.post(
+		"/auth/signin",
+		passport.authenticate("local-signin", {
+			successRedirect: "/newStory",
+			failureRedirect: "/auth/signup"
+		})
+	);
+};
